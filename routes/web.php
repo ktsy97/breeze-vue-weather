6bbccdcc2eabd6ad808eee5f350f9e12f8932365
KONDO Katsuya <k0d0ktsy@gmail.com>
2022-05-22 09:10:53 +0900

    ルーティングを追加

--- routes/web.php
+++ routes/web.php
@@ -28,4 +28,6 @@ Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/list', [App\Http\Controllers\WeatherController::class, 'index'])->name('city.list');
+
 require __DIR__.'/auth.php';
