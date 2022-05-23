f46e5b317020d91a06934a146766d05d72f0fe64
KONDO Katsuya <k0d0ktsy@gmail.com>
2022-05-23 15:49:03 +0900

    外部APIから現在の天気を取得

--- app/Http/Controllers/WeatherController.php
+++ app/Http/Controllers/WeatherController.php
@@ -4,13 +4,29 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Inertia\Inertia;
+use GuzzleHttp\Client;
 
 class WeatherController extends Controller
 {
     public function index(Request $request)
     {
+
+        $cityNames = ['東京都', '愛知県', '大阪府'];
+
+        $apiKey = config('services.weather.key');
+
+        $method = "GET";
+        $client = new Client();
+
+        foreach ($cityNames as $cityName) {
+            $url = "http://api.openweathermap.org/data/2.5/weather?units=metric&lang=ja&q=$cityName&appid=$apiKey";
+            $response = $client->request($method, $url);
+            $data[] = json_decode($response->getBody(), true);
+        }
+
         return Inertia::render(
             'CityList',
+            ['data' => $data],
         );
     }
 
