4f8e14441540c218368a9dbaa4a521ba609edee8
KONDO Katsuya <k0d0ktsy@gmail.com>
2022-05-28 14:37:48 +0900

    ルーティング変更

--- routes/web.php
+++ routes/web.php
@@ -15,13 +15,13 @@ use Inertia\Inertia;
 |
 */
 
-Route::get('/', [App\Http\Controllers\WeatherController::class, 'index'])->middleware(['auth', 'verified'])->name('city.list');
-Route::post('/', [App\Http\Controllers\WeatherController::class, 'index'])->name('city.select');
-Route::get('/city/{city_id}', [App\Http\Controllers\WeatherController::class, 'show'])->middleware(['auth', 'verified'])->name('city.show');
-Route::post('/city/fav', [App\Http\Controllers\FavoriteController::class, 'fav'])->name('city.fav');
+Route::get('/', [App\Http\Controllers\WeatherController::class, 'index'])->middleware(['auth'])->name('city.list');
+Route::post('/', [App\Http\Controllers\WeatherController::class, 'index'])->middleware(['auth'])->name('city.select');
+Route::get('/city/{city_id}', [App\Http\Controllers\WeatherController::class, 'show'])->middleware(['auth'])->name('city.show');
+Route::post('/city/fav', [App\Http\Controllers\FavoriteController::class, 'fav'])->middleware(['auth'])->name('city.fav');
 
-Route::get('/user', [App\Http\Controllers\UserController::class, 'show'])->middleware(['auth', 'verified'])->name('user.show');
-Route::post('/user', [App\Http\Controllers\UserController::class, 'update'])->middleware(['auth', 'verified'])->name('user.update');
-Route::delete('/user', [App\Http\Controllers\UserController::class, 'destroy'])->middleware(['auth', 'verified'])->name('user.destroy');
+Route::get('/user', [App\Http\Controllers\UserController::class, 'show'])->middleware(['auth'])->name('user.show');
+Route::post('/user/{user}', [App\Http\Controllers\UserController::class, 'update'])->middleware(['auth'])->name('user.update');
+Route::delete('/user/{user}', [App\Http\Controllers\UserController::class, 'destroy'])->middleware(['auth'])->name('user.destroy');
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
